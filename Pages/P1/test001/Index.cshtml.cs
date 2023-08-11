7292e3804a72cb16ac103ec1abc3a3a43282f618
AllenLee-AL <133303654+AllenLee-AL@users.noreply.github.com>
2023-08-11 11:17:27 +0800

    modify P1 add test002

--- Pages/P1/test001/Index.cshtml.cs
+++ Pages/P1/test001/Index.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace jenkins_mvc_demo.Pages.P1
+namespace jenkins_mvc_demo.Pages.P1.test001
 {
     public class IndexModel : PageModel
     {
